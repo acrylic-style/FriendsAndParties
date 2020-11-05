8b50a4a3120f489b81b503f9680d105cfc3de88c
acrylic-style <me@acrylicstyle.xyz>
2020-11-05 22:14:39 +0900

    /pl conflicts with /bukkit:pl

--- src/main/kotlin/xyz/acrylicstyle/fap/commands/PartyListCommand.kt
+++ src/main/kotlin/xyz/acrylicstyle/fap/commands/PartyListCommand.kt
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.plugin.Command
 
-class PartyListCommand: Command("partylist", null, "pl") { // replacing /pl in bukkit is intentional
+class PartyListCommand: Command("partylist", null, "plist") { // replacing /pl in bukkit is intentional
     override fun execute(sender: CommandSender, args: Array<out String>) {
         if (sender is ProxiedPlayer) PartyCommand.doList(sender)
     }
